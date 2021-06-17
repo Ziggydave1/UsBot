51f3365e74d9534778dbd5f7b7659ddf6cff5640
Henry Pontzer <henry@jpontzer.net>
2021-06-16 22:30:34 -0600

    Bug fixes

--- commands/fun/lfg.js
+++ commands/fun/lfg.js
@@ -18,7 +18,7 @@ module.exports = {
         //member is of type [Snowflake, GuildMember]
         for (const member of members) {
             if (member[0] !== message.author.id && gamePlayers.players.find(p => p.id === member[0])) {
-                if (gamePlayers.players.find(p => p.id === member[0]).games.includes(game.id.toLowerCase()) !== -1) {
+                if (gamePlayers.players.find(p => p.id === member[0]).games.includes(game.id.toLowerCase())) {
                     reply += member[1].user.toString();
                 }
             }
