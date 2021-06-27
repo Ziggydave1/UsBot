f3d017fb1625298b8828f4a8fc02587fa1587dfa
Henry Pontzer <henry@jpontzer.net>
2021-06-27 12:38:43 -0600

    Refactored mygames and implemented helper methods

--- commands/fun/mygames.js
+++ commands/fun/mygames.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const gameHelper = require('../../helpers/gameHelper');
 module.exports = {
     name: 'mygames',
     description: 'allows a user to view and manage their owned games',
@@ -10,7 +11,7 @@ module.exports = {
         const gamePlayers = require('./gamePlayers.json');
 
         //targetedMember is type GuildMember
-        const targetedMember = message.mentions.members.size === 0 ? message.member : message.mentions.members.first();
+        const targetedMember = message.mentions.members.first() ?? message.member;
 
         let player = gamePlayers.players.find(entry => entry.id === targetedMember.user.id);
         if (!player) {
@@ -33,89 +34,131 @@ module.exports = {
         
         switch (args[0]) {
             case ('viewall'):
-                let reply = "**Games:**";
-                for (const game of gameList.games) {
-                    let emoji = message.guild.emojis.cache.find(e => e.name === game.id);
-                if (!emoji) {
-                    await message.guild.emojis.create(`./assets/emoji/${game.id}.png`, game.id);
-                    emoji = message.guild.emojis.cache.find(e => e.name === game.id);
-                }
-                reply += `\n${emoji} *\`${game.name}\`*`;
-                };
-                message.channel.send(reply);
+                await listAllGamesAsync();
                 break;
             case ('view'):
-                //This displays user profile info
-                let playerGames = '';
-                player.games.forEach(async gameName => {
-                    game = gameList.games.find(entry => entry.id.toLowerCase() === gameName.toLowerCase());
-                    let emoji = message.guild.emojis.cache.find(e => e.name === game.id);
-                    if (!emoji) {
-                        await message.guild.emojis.create(`./assets/emoji/${game.id}.png`, game.id);
-                        emoji = message.guild.emojis.cache.find(e => e.name === game.id);
-                    }
-                    playerGames = playerGames + `${emoji} ${game.name} \n`;
-                });
-                const newEmbed = new Discord.MessageEmbed()
-                    .setColor('#FFFFFF')
-                    .setTitle(targetedMember.user.username)
-                    .setThumbnail(targetedMember.user.displayAvatarURL())
-                    .addField('Games', playerGames ? playerGames : 'None')
-                message.channel.send(newEmbed);
+                await viewPlayerGamesAsync();
                 break;
             case ('add'):
-                //This adds a game to a user's collection
-                if (!args[1]) {
-                    message.channel.send('Correct usage is \`-mygames add <game name>\`');
-                    break;
-                }
-
-                game = gameList.games.find(entry => (entry.id.toLowerCase() === args[1].toLowerCase()) || (entry.aliases.includes(args[1].toLowerCase())));
-
-                if (!game) {
-                    message.channel.send('Game not found');
-                    break;
-                }
-
-                if (player.games.includes(game.id.toLowerCase())) {
-                    message.channel.send('Game already added');
-                    break;
-                }
-                player.games.push(game.id.toLowerCase());
-                jsonString = JSON.stringify(gamePlayers, null, 4);
-                fs.writeFile('./commands/fun/gamePlayers.json', jsonString, err => {
-                    if (err) {
-                        message.channel.send('Error adding game');
-                        console.log('Error adding game', err);
-                    } else {
-                        message.channel.send('Game added');
-                    }
-                })
+                addGame();
                 break;
             case ('remove'):
-                //This removes a game from a user's collection    
-                if (!args[1]) {
-                    message.channel.send('Correct usage is \`-mygames remove <game name>\`');
-                    break;
-                }
-
-                game = gameList.games.find(entry => (entry.id.toLowerCase() === args[1].toLowerCase()) || (entry.aliases.includes(args[1].toLowerCase())));
-
-                if (!player.games.includes(game.id.toLowerCase())) {
-                    message.channel.send('Game not owned');
-                    break;
-                }
-                player.games.splice(player.games.indexOf(game.id.toLowerCase()), 1);
-                jsonString = JSON.stringify(gamePlayers, null, 4);
-                fs.writeFile('./commands/fun/gamePlayers.json', jsonString, err => {
-                    if (err) {
-                        message.channel.send('Error removing game');
-                        console.log('Error removing game', err);
-                    } else {
-                        message.channel.send('Game removed');
-                    }
-                })
+                removeGame();
                 break;
+        }
+
+        async function listAllGamesAsync() {
+            let gamesMessage = '';
+            for (const game of gameList.games) {
+                const emoji = await gameHelper.getEmojiAsync(game, message);
+                gamesMessage += `\n${emoji} *\`${game.name}\`*`;
+            }
+            const gamesEmbed = new Discord.MessageEmbed()
+                .setColor('#C792EA')
+                .setTitle('Games')
+                .setDescription(gamesMessage);
+            message.channel.send(gamesEmbed);
+        }
+
+        async function viewPlayerGamesAsync() {
+            let playerGames = '';
+
+            for (const gameName of player.games)
+            {
+                game = gameHelper.findGame(gameName);
+                let emoji = await gameHelper.getEmojiAsync(game, message);
+                playerGames = playerGames + `${emoji} ${game.name} \n`;
+            }
+            const newEmbed = new Discord.MessageEmbed()
+                .setColor('#FFB116')
+                .setAuthor(targetedMember.user.username, targetedMember.user.displayAvatarURL())
+                .addField('Games', playerGames ? playerGames : 'None')
+            message.channel.send(newEmbed);
+        }
+
+        function addGame() {
+            if (!args[1]) {
+                const noGameEmbed = new Discord.MessageEmbed()
+                    .setColor('#E01F01')
+                    .setTitle('Error: No game mentioned')
+                    .setFooter('UsBot', client.user.displayAvatarURL())
+                    .setAuthor(message.author.username, message.author.displayAvatarURL())
+                    .setDescription('Correct usage is \`-mygames add <game>\`');
+                message.channel.send(noGameEmbed);
+                return;
+            }
+
+            game = gameHelper.findGame(args[1]);
+
+            if (player.games.includes(game.id.toLowerCase())) {
+                const gameOwnedEmbed = new Discord.MessageEmbed()
+                    .setColor('#389D59')
+                    .setTitle('Game already owned')
+                    .setFooter('UsBot', client.user.displayAvatarURL())
+                    .setAuthor(message.author.username, message.author.displayAvatarURL())
+                    .setDescription(`${targetedMember.user.username} already has **${game.name}** in their collection!`)
+                message.channel.send(gameOwnedEmbed);
+                return;
+            }
+            player.games.push(game.id.toLowerCase());
+            if (updateGamePlayersFile()) {
+                const gameAddedEmbed = new Discord.MessageEmbed()
+                    .setColor('#389D59')
+                    .setTitle('Game added')
+                    .setFooter('UsBot', client.user.displayAvatarURL())
+                    .setAuthor(message.author.username, message.author.displayAvatarURL())
+                    .setDescription(`**${game.name}** has been added to ${targetedMember.user.username}'s collection.`)
+                message.channel.send(gameAddedEmbed);
+            }
+        }
+
+        function removeGame() {
+            if (!args[1]) {
+                const noGameEmbed = new Discord.MessageEmbed()
+                    .setColor('#E01F01')
+                    .setTitle('Error: No game mentioned')
+                    .setFooter('UsBot', client.user.displayAvatarURL())
+                    .setAuthor(message.author.username, message.author.displayAvatarURL())
+                    .setDescription('Correct usage is \`-mygames remove <game>\`');
+                message.channel.send(noGameEmbed);
+                return;
+            }
+
+            game = gameHelper.findGame(args[1]);
+
+            if (!player.games.includes(game.id.toLowerCase())) {
+                const gameNotOwnedEmbed = new Discord.MessageEmbed()
+                    .setColor('#389D59')
+                    .setTitle('Game not owned')
+                    .setFooter('UsBot', client.user.displayAvatarURL())
+                    .setAuthor(message.author.username, message.author.displayAvatarURL())
+                    .setDescription(`${targetedMember.user.username} doesn't have **${game.name}** in their collection!`)
+                message.channel.send(gameNotOwnedEmbed);
+                return;
+            }
+            player.games.splice(player.games.indexOf(game.id.toLowerCase()), 1);
+            if (updateGamePlayersFile()) {
+                const gameRemovedEmbed = new Discord.MessageEmbed()
+                    .setColor('#389D59')
+                    .setTitle('Game removed')
+                    .setFooter('UsBot', client.user.displayAvatarURL())
+                    .setAuthor(message.author.username, message.author.displayAvatarURL())
+                    .setDescription(`**${game.name}** has been removed from ${targetedMember.user.username}'s collection.`)
+                message.channel.send(gameRemovedEmbed);
+            }
+        }
+
+        function updateGamePlayersFile() {
+            jsonString = JSON.stringify(gamePlayers, null, 4);
+            fs.writeFile('./commands/fun/gamePlayers.json', jsonString, err => {
+                if (err) {
+                    console.log('Error adding game', err);
+                    return false;
+                } else {
+                    return true;
+                }
+            });
+            return true;
         }
     }
 }
