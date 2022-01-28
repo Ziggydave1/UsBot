d0853c1422f3e25b7f8f3dcd9e250b1c31614994
Henry Pontzer <henry@jpontzer.net>
2022-01-27 18:45:01 -0700

    Added combined guild and global deployment

--- deploy-commands.ts
+++ deploy-commands.ts
@@ -15,16 +15,32 @@ console.log('📂 Reading command folder...');
 const commandFiles = readdirSync('./commands').filter(file => file.endsWith('.ts'));
 console.log('📁 Read command folder with', commandFiles.length, 'commands.');
 
-console.log('🔎 Reading commands...');
+console.log('📖 Reading commands...');
 for (const file of commandFiles) {
 	const loadedCommand = await import(`./commands/${file}`);
 	const command: Command = new loadedCommand.default();
 	commands.push(command.data.toJSON());
 }
-console.log('✅ Read', commands.length, 'commands.');
+console.log('🧠 Read', commands.length, 'commands.');
 
 const rest = new REST({ version: '9' }).setToken(config.token);
 
-rest.put(Routes.applicationGuildCommands(config.clientId, config.guildId), { body: commands })
-	.then(() => console.log('✅ Successfully registered application commands.'))
-	.catch(console.error);
+(async () => {
+	try {
+		console.log('🔃 Started refreshing application (/) commands.');
+
+		await rest.put(
+			Routes.applicationGuildCommands(config.clientId, config.guildId),
+			{ body: commands },
+		);
+		
+		await rest.put(
+			Routes.applicationCommands(config.clientId),
+			{ body: commands },
+		);
+
+		console.log('✅ Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
